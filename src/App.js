343946fdb4db105ae3675d7bfb3d63dbdc622ad1
Harry2k21 <88850029+Harry2k21@users.noreply.github.com>
2023-02-04 22:09:23 +0000

    Add files via upload
    New Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Dino  from './assets/dino.png';
+import './Factpage.js'
 
 function App() {
   return (
@@ -8,9 +9,9 @@ function App() {
       <h1>Jerry is hungry, feed him an image. </h1>
       <img src={Dino} alt="dino" />
       <div class="Logo"></div>
-      {/* <Dino/> */}
       
-      <button>Upload Image</button>
+      
+      <button >Upload Image</button>
     </div>
   );
 }
